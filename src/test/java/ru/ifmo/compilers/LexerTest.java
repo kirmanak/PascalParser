0392b1e0d1a3b83ca147f5b447f04e1b05dffa22
Kirill Kamakin <k.kamakin@protonmail.ch>
2019-04-24 09:43:01 +0300

    Add a big test program
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- src/test/java/ru/ifmo/compilers/LexerTest.java
+++ src/test/java/ru/ifmo/compilers/LexerTest.java
@@ -31,4 +31,111 @@ class LexerTest {
 
         assertIterableEquals(expected, getResult(code));
     }
+
+    @Test
+    void bigProgram() {
+        final var code =
+                "Var i, abc, d;\n" + // 1
+                        "Begin\n" + // 2
+                        "  i:=10;\n" + // 3
+                        "  abc := i;\n" + // 4
+                        "  d:=abc,i:=i-10;\n" + // 5
+                        "  WHILE i<5 DO\n" + // 6
+                        "  Begin\n" + // 7
+                        "    i:=i*2;\n" + // 8
+                        "  End\n" + // 9
+                        "  abc:= d/(i+2);\n" + // 10
+                        "End."; // 11
+
+        final var expected = List.of(
+                new Lexeme(LexemeClass.Keyword, "Var", 1),
+                new Lexeme(LexemeClass.Separator, " ", 1),
+                new Lexeme(LexemeClass.Ident, "i", 1),
+                new Lexeme(LexemeClass.Separator, ",", 1),
+                new Lexeme(LexemeClass.Separator, " ", 1),
+                new Lexeme(LexemeClass.Ident, "abc", 1),
+                new Lexeme(LexemeClass.Separator, ",", 1),
+                new Lexeme(LexemeClass.Separator, " ", 1),
+                new Lexeme(LexemeClass.Ident, "d", 1),
+                new Lexeme(LexemeClass.Separator, ";", 1),
+
+                new Lexeme(LexemeClass.Keyword, "Begin", 2),
+
+                new Lexeme(LexemeClass.Separator, " ", 3),
+                new Lexeme(LexemeClass.Separator, " ", 3),
+                new Lexeme(LexemeClass.Ident, "i", 3),
+                new Lexeme(LexemeClass.AssignmentOperator, ":=", 3),
+                new Lexeme(LexemeClass.Const, "10", 3),
+                new Lexeme(LexemeClass.Separator, ";", 3),
+
+                new Lexeme(LexemeClass.Separator, " ", 4),
+                new Lexeme(LexemeClass.Separator, " ", 4),
+                new Lexeme(LexemeClass.Ident, "abc", 4),
+                new Lexeme(LexemeClass.Separator, " ", 4),
+                new Lexeme(LexemeClass.AssignmentOperator, ":=", 4),
+                new Lexeme(LexemeClass.Separator, " ", 4),
+                new Lexeme(LexemeClass.Ident, "i", 4),
+                new Lexeme(LexemeClass.Separator, ";", 4),
+
+                new Lexeme(LexemeClass.Separator, " ", 5),
+                new Lexeme(LexemeClass.Separator, " ", 5),
+                new Lexeme(LexemeClass.Ident, "d", 5),
+                new Lexeme(LexemeClass.AssignmentOperator, ":=", 5),
+                new Lexeme(LexemeClass.Ident, "abc", 5),
+                new Lexeme(LexemeClass.Separator, ",", 5),
+                new Lexeme(LexemeClass.Ident, "i", 5),
+                new Lexeme(LexemeClass.AssignmentOperator, ":=", 5),
+                new Lexeme(LexemeClass.Ident, "i", 5),
+                new Lexeme(LexemeClass.ArithmeticOperator, "-", 5),
+                new Lexeme(LexemeClass.Const, "10", 5),
+                new Lexeme(LexemeClass.Separator, ";", 5),
+
+                new Lexeme(LexemeClass.Separator, " ", 6),
+                new Lexeme(LexemeClass.Separator, " ", 6),
+                new Lexeme(LexemeClass.Keyword, "WHILE", 6),
+                new Lexeme(LexemeClass.Separator, " ", 6),
+                new Lexeme(LexemeClass.Ident, "i", 6),
+                new Lexeme(LexemeClass.ComparisonOperator, "<", 6),
+                new Lexeme(LexemeClass.Const, "5", 6),
+                new Lexeme(LexemeClass.Separator, " ", 6),
+                new Lexeme(LexemeClass.Keyword, "DO", 6),
+
+                new Lexeme(LexemeClass.Separator, " ", 7),
+                new Lexeme(LexemeClass.Separator, " ", 7),
+                new Lexeme(LexemeClass.Keyword, "Begin", 7),
+
+                new Lexeme(LexemeClass.Separator, " ", 8),
+                new Lexeme(LexemeClass.Separator, " ", 8),
+                new Lexeme(LexemeClass.Separator, " ", 8),
+                new Lexeme(LexemeClass.Separator, " ", 8),
+                new Lexeme(LexemeClass.Ident, "i", 8),
+                new Lexeme(LexemeClass.AssignmentOperator, ":=", 8),
+                new Lexeme(LexemeClass.Ident, "i", 8),
+                new Lexeme(LexemeClass.ArithmeticOperator, "*", 8),
+                new Lexeme(LexemeClass.Const, "2", 8),
+                new Lexeme(LexemeClass.Separator, ";", 8),
+
+                new Lexeme(LexemeClass.Separator, " ", 9),
+                new Lexeme(LexemeClass.Separator, " ", 9),
+                new Lexeme(LexemeClass.Keyword, "End", 9),
+
+                new Lexeme(LexemeClass.Separator, " ", 10),
+                new Lexeme(LexemeClass.Separator, " ", 10),
+                new Lexeme(LexemeClass.Ident, "abc", 10),
+                new Lexeme(LexemeClass.AssignmentOperator, ":=", 10),
+                new Lexeme(LexemeClass.Separator, " ", 10),
+                new Lexeme(LexemeClass.Ident, "d", 10),
+                new Lexeme(LexemeClass.ArithmeticOperator, "/", 10),
+                new Lexeme(LexemeClass.Separator, "(", 10),
+                new Lexeme(LexemeClass.Ident, "i", 10),
+                new Lexeme(LexemeClass.ArithmeticOperator, "+", 10),
+                new Lexeme(LexemeClass.Const, "2", 10),
+                new Lexeme(LexemeClass.Separator, ")", 10),
+                new Lexeme(LexemeClass.Separator, ";", 10),
+
+                new Lexeme(LexemeClass.Keyword, "End.", 11)
+        );
+
+        assertIterableEquals(expected, getResult(code));
+    }
 }
