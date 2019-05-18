91b18b1d0de3d6dbcd1e2b022371b79951f18581
Kirill Kamakin <k.kamakin@protonmail.ch>
2019-05-18 12:31:56 +0300

    Use Lombok
    Signed-off-by: Kirill Kamakin <k.kamakin@protonmail.ch>

--- src/main/java/ru/ifmo/compilers/Lexeme.java
+++ src/main/java/ru/ifmo/compilers/Lexeme.java
@@ -1,72 +1,27 @@
 package ru.ifmo.compilers;
 
+import lombok.Data;
+import lombok.NonNull;
+
 /**
  * Represents a lexeme found in the source code
  */
+@Data
 public class Lexeme {
     /**
      * The class of found lexeme
      */
+    @NonNull
     private final LexemeClass mClass;
 
     /**
      * The sign of the lexeme
      */
+    @NonNull
     private final String mSign;
 
     /**
      * The line of source code where it was found
      */
     private final int mLine;
-
-    Lexeme(LexemeClass mClass, String mSign, int mLine) {
-        if (mClass == null || mSign == null || mSign.length() == 0)
-            throw new IllegalArgumentException("Lexeme can not contain empty values");
-
-        this.mClass = mClass;
-        this.mSign = mSign;
-        this.mLine = mLine;
-    }
-
-    public LexemeClass getLexemeClass() {
-        return mClass;
-    }
-
-    public String getSign() {
-        return mSign;
-    }
-
-    public int getLine() {
-        return mLine;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("Lexeme { mClass = %-20s, mSign = '%-10s', mLine = %-3d }", mClass, mSign, mLine);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-
-        Lexeme lexeme = (Lexeme) o;
-
-        if (mLine != lexeme.mLine)
-            return false;
-        if (mClass != lexeme.mClass)
-            return false;
-        return mSign.equals(lexeme.mSign);
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = mClass.hashCode();
-        result = 31 * result + mSign.hashCode();
-        result = 31 * result + mLine;
-        return result;
-    }
 }
